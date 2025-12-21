6664961c6732d047c3e09a7e4efa81bc3a6e9f0f
jaemin <als982001@naver.com>
2025-12-21 16:49:34 +0900

    viteconfig 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { reactRouter } from '@react-router/dev/vite'
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { reactRouter } from "@react-router/dev/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths()],
-})
+  build: {
+    outDir: "dist", // 기본값
+  },
+});
