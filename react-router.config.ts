ae9837ad171b9c520d71ac2899813e88dfef0e4a
jaemin <als982001@naver.com>
2026-01-18 15:38:53 +0900

    Revert "feat: Vercel React Router 어댑터 적용"
    This reverts commit 6f83e0059fc612ce657c2b7a7c6eba28ba5ffbcd.

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "@react-router/dev/config";
-import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
-  presets: [vercelPreset()],
+  ssr: false,
+  prerender: ["/", "/history", "/login"],
 } satisfies Config;
